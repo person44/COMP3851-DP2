d3297db63d43a7703a272f92d96f0046944e2536
Jun Wei <96361520+thejonnyjon@users.noreply.github.com>
2022-02-22 17:03:50 +0800

    Update Home.js

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -12,7 +12,7 @@ function Home() {
         <Header />
         <div style={{ backgroundImage: `url('${headerImage}')`, backgroundSize: 'cover', height: '50vh' }}>
             <div class="animate__animated animate__zoomIn" style={{ fontWeight: 'bolder', fontSize: '230%', color: 'white', position: 'absolute', top: '18%', left: '15%' }}>
-                Hello and Welcome to the University of Newcastle, Australia
+                Hello and Welcome to the University of Newcastle, Australia!
             </div>
             <div style={{ fontSize: 10, color: 'white', position: 'absolute', top: '48vh', right: 0 }}>
                 Image from: <a href="https://www.newcastle.edu.au/study/international">UON Australia</a>
