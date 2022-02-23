b9ec362d406fb3a1ccd890c9c6c184d82f090fe3
Jun Wei <96361520+thejonnyjon@users.noreply.github.com>
2022-02-23 11:30:37 +0800

    Updated fontSize

--- src/Page/Welcome.js
+++ src/Page/Welcome.js
@@ -18,7 +18,7 @@ function Welcome() {
         color: "#fff",
         backgroundColor: "#000",
         height: "100vh",
-        fontSize: "5vw",
+        fontSize: "4vw",
         fontFamily: "monospace",
         padding: "5vw"
       }}
@@ -26,9 +26,10 @@ function Welcome() {
         <Typist>
           <p style={ heading }> Welcome to UON iPath! </p>
           <Typist.Delay ms={50} />
-          We help you to find your perfect pathway from a freshman to the day you graduate. TRY US!
-              </Typist>
-              <Button variant="success" href="./Login">SKIP AHEAD AND LOGIN</Button>
+          We help you to find your perfect pathway from a freshman to the day you graduate. TRY US! <br />
+          </Typist>
+          
+          <Button variant="success" href="./Login">SKIP AHEAD AND LOGIN</Button>
     </div>
   );
 }
