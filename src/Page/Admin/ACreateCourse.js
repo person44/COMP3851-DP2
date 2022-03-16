d419a76f552c120c239b492aac7f6e637a1ec51b
person44 <99407176+person44@users.noreply.github.com>
2022-03-16 21:39:42 +0800

    Update ACreateCourse.js

--- src/Page/Admin/ACreateCourse.js
+++ src/Page/Admin/ACreateCourse.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Col, Form, Row, Button, Container } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
 import Header from '../Partials/Header'
-function ACreateCourse() {
+import { db } from '../../firebaseDB'
+import { ref, set } from 'firebase/database'
+
+function addCourse(courseCo, courseAv, courseLo, courseNa, coursePr){
+    set(ref(db, "Course/" + courseCo), {
+        Availability: courseAv,
+        Location: courseLo,
+        Name: courseNa,
+        Prerequisite: coursePr
+    })
+    .then(()=>{
+        alert("Course Added");
+    })
+    .catch(()=>{
+        alert("Course Not Added");
+    })
+}
+
+function ACreateCourse(){
+    var [courseCode, setCode]=useState();
+    var [courseName, setName ]=useState();
+    var [courseLocation, setLoc]=useState();
+    var [courseAvailability, setAv]=useState();
+    var [coursePrerequisite, setPre]=useState();
+    
     return (
         <>
             <Header />
@@ -11,62 +34,50 @@ function ACreateCourse() {
                 <Form className=" mt-5 text-left create-form">
                     <Form.Group as={Row} className="form-group">
                         <Col sm="2" className="text-left">
-                            <Form.Label className="mt-2">Course Name:</Form.Label>
+                            <Form.Label className="mt-2">Course Code:</Form.Label>
                         </Col>
                         <Col sm="6">
-                            <Form.Control />
+                        <   input type="text" value={courseCode} onInput={e => setCode(e.target.value)}></input>
                         </Col>
                     </Form.Group>
                     <Form.Group as={Row} className="form-group">
                         <Col sm="2" className="text-left">
-                            <Form.Label className="mt-2">Course Code:</Form.Label>
+                            <Form.Label className="mt-2">Course Name:</Form.Label>
                         </Col>
                         <Col sm="3">
-                            <Form.Control />
+                            <input type="text" value={courseName} onInput={e => setName(e.target.value)}></input>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="form-group">
+                        <Col sm="2" className="text-left">
+                            <Form.Label className="mt-2">Location:</Form.Label>
                         </Col>
                         <Col sm="3">
-                            <Form.Control />
+                            <input type="text" value={courseLocation} onInput={e => setLoc(e.target.value)}></input>
                         </Col>
                     </Form.Group>
-                    <Form.Group className="mt-2" as={Row} className="form-group">
+                    <Form.Group as={Row} className="form-group">
                         <Col sm="2" className="text-left">
-                            <Form.Label className="mt-2">Degree</Form.Label>
+                            <Form.Label className="mt-2">Availability:</Form.Label>
                         </Col>
-                        <Col sm="6">
-                            <Form.Control as="select">
-                                <option value="NaN">Select Degree</option>
-                            </Form.Control>
+                        <Col sm="3">
+                            <input type="text" value={courseAvailability} onInput={e => setAv(e.target.value)}></input>
                         </Col>
                     </Form.Group>
-                    <Form.Group className="mt-2" as={Row} className="form-group">
+                    <Form.Group as={Row} className="form-group">
                         <Col sm="2" className="text-left">
-                            <Form.Label className="mt-2">Majors</Form.Label>
+                            <Form.Label className="mt-2">Prerequisite:</Form.Label>
                         </Col>
-                        <Col sm="6">
-                            <Form.Control as="select" multiple>
-                                <option value="NaN"> &lt; Major 1 &gt;</option>
-                                <option value="NaN"> &lt; Major 2 &gt;</option>
-                                <option value="NaN"> &lt; Major 3 &gt;</option>
-                            </Form.Control>
+                        <Col sm="3">
+                            <input type="text" value={coursePrerequisite} onInput={e => setPre(e.target.value)}></input>
                         </Col>
                     </Form.Group>
-                    <Form.Group className="mt-2" as={Row} className="form-group">
-                        <Col sm="2" className="text-left">
-                            <Form.Label className="mt-2">Required Course</Form.Label>
-                        </Col>
-                        <Col sm="6">
-                            <Form.Control as="select" multiple>
-                                <option value="NaN"> &lt; Course 1 &gt;</option>
-                            </Form.Control>
-                        </Col>
-                    </Form.Group>
-                    
-
                     <Form.Group className="mt-2" as={Row}>
                         <Col sm="4"></Col>
 
                         <Col sm="2" className="text-left">
-                            <Link className="btn btn-lg" to="/admin/success"> Add Course</Link>
+                            <button id="addBtn" onClick={()=> addCourse(courseCode, courseAvailability, courseLocation, courseName, coursePrerequisite)}>Add Course</button>
+                            {/* <Link className="btn btn-lg" to="/admin/success"> Add Course</Link> */}
                         </Col>
                     </Form.Group>
                 </Form>
